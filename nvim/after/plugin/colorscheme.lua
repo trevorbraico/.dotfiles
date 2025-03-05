688cebdbfe4d67ac65fe72940c4f7f8fa2329a24
Trevor Braico <tbraico.github@gmail.com>
2025-03-04 20:31:52 -0600

    updated neovim default opacity

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,12 +1,17 @@
 require("tokyonight").setup({ style = "night" })
 vim.cmd.colorscheme('tokyonight')
 
-local function transparent()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+local current_opacity = 100
+
+local function toggleTransparency()
+    current_opacity = 100 - current_opacity
+    vim.api.nvim_set_hl(current_opacity, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(current_opacity, "NormalFloat", { bg = "none" })
 end
 
-vim.api.nvim_create_user_command("Transparent", transparent, {
+vim.api.nvim_create_user_command("ToggleTransparency", toggleTransparency, {
     nargs = 0,
-    desc = "Make the background transparent for all Neovim windows",
+    desc = "Toggle between full and no transparency in windows",
 })
+
+toggleTransparency()
