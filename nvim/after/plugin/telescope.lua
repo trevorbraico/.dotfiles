76894b1b22088637acb77aa494bf9fa6e186d37f
Trevor Braico <braicotrevor@gmail.com>
2025-06-15 08:39:05 -0500

    added man pages search keybind

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,9 @@
 local builtins = require("telescope.builtin")
-local sorters = require("telescope.sorters")
 
 vim.keymap.set("n", "<leader>pf", builtins.find_files, {})
 vim.keymap.set("n", "<leader>gf", builtins.git_files, {})
 vim.keymap.set("n", "<leader>ps", builtins.live_grep, {})
+vim.keymap.set("n", "<leader>ma", builtins.man_pages, {})
 vim.keymap.set("n", "<leader>cm", builtins.commands, {})
 vim.keymap.set("n", "<leader>co", builtins.colorscheme, {})
 vim.keymap.set("n", "<leader>sh", builtins.keymaps, {})
